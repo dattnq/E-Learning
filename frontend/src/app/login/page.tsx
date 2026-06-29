43b2297e321ce08effe70fc133a38f224cc8e8d2
Dattnq <trieudat3107@gmail.com>
2026-06-29 12:24:53 +0700

    update structure

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">Đăng nhập hệ thống</h1>
+      {/* Form nhập email, password sẽ code ở đây sau */}
+    </div>
+  );
+}
